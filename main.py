0eff8048fa12367541040687022ec87eba4188aa
matthieu <66738778+Matthieudese78@users.noreply.github.com>
2024-10-30 09:32:20 +0100

    main..py : creation

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/home/matthieu/Documents/dotblocks/dotb_case_study/myenv/python3
+# %% Loading packages :
+# Standard packages :
+from __future__ import annotations
+
+# Local packages :
+
+
+# %% main function definition :
+def main():
+    print('This is the main program')
+
+
+# %% direct execution :
+if __name__ == '__main__':
+    main()
+else:
+    print('This script was imported')
