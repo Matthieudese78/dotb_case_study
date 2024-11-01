da9945a06e144dd0c780a9499a35ffe574d2fa0b
matthieu <66738778+Matthieudese78@users.noreply.github.com>
2024-11-01 19:17:27 +0100

    dotb/input_yaml.py : function load_config(file_path)
    - parses the arguments of python main.py {args}
    - if no arg or erroneous arg : uses ballistic_default_input.yaml instead

--- dotb/input_yaml.py
+++ dotb/input_yaml.py
@@ -1,4 +1,38 @@
 #!/usr/bin/python3
-import yaml
-import os
+from __future__ import annotations
+
 import argparse
+import os
+
+# import yaml
+# find
+
+
+def get_config_file(default_config_path):
+    # Define the default config file path
+    # default_config_path = os.path.join(os.path.dirname(__file__), 'default_config.yaml')
+    default_config_path = (
+        f'{default_config_path}/ballistic_default_input.yaml'
+    )
+
+    # Create an ArgumentParser instance
+    parser = argparse.ArgumentParser(description='Your script description')
+    parser.add_argument(
+        'config', nargs='?', default=default_config_path,
+        help='Path to the config file (optional)',
+    )
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Get the config file path
+    config_path = args.config
+
+    # Check if the provided config file exists
+    if not os.path.exists(config_path):
+        print(
+            f"Warning: Config file '{config_path}' does not exist. Using default config.",
+        )
+        config_path = default_config_path
+
+    return config_path
